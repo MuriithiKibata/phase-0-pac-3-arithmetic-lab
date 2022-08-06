c7afedfaf4238aa1420675b26db91f7292c90156
Muriithi <muriithi.kibata@student.moringaschool.com>
2022-08-06 16:03:04 +0300

    Completed Assignment

---
+++ index.js
@@ -0,0 +1,47 @@
+ let a = 4
+ let b = 4
+ function add (a,b) {
+    console.log (a+b)
+ }
+function subtract(a,b) {
+    console.log (a-b)
+}
+function multiply (a,b) {
+    console.log(a*b)
+}
+function divide (a,b) {
+    console.log(a/b)
+}
+function add (a,b) {
+    return a+b
+}
+function subtract (a,b) {
+    return a - b
+}
+function multiply (a,b) {
+    return a * b
+}
+function divide (a,b) {
+    return a/b
+}
+let n = '208'
+function increment (n) {
+    return  ++n
+}
+function decrement (n) {
+    return --n
+}
+function makeInt(n) {
+    return parseInt(n,10)
+}
+function preserveDecimal (n) {
+    return parseFloat(n) 
+}
+
+
+
+
+ 
+
+  
+
